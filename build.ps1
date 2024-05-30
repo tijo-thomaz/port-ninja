1d9b9c07836162845e11867b0f4285833d96465e
Tijo Thomas <tijo1293@gmail.com>
2024-05-30 16:19:50 +0100

    HotFix(workflow) - update the workflow

--- build.ps1
+++ build.ps1
@@ -1,23 +1,19 @@
 # build.ps1
 
-# Create directories for storing built binaries
-$directories = @(".build/release/linux", ".build/release/darwin", ".build/release/windows")
-foreach ($directory in $directories) {
-    if (-not (Test-Path $directory -PathType Container)) {
-        New-Item -ItemType Directory -Path $directory | Out-Null
-    }
-}
-
-# Define build targets
 $targets = @(
     @{ GOOS = "linux"; GOARCH = "amd64"; Output = "port_ninja_linux_amd64" },
     @{ GOOS = "darwin"; GOARCH = "amd64"; Output = "port_ninja_darwin_amd64" },
     @{ GOOS = "windows"; GOARCH = "amd64"; Output = "port_ninja_windows_amd64.exe" }
 )
 
-# Iterate over each target and build the application
 foreach ($target in $targets) {
     $env:GOOS = $target.GOOS
     $env:GOARCH = $target.GOARCH
-    go build -o ".build/release/$($target.GOOS)/$($target.Output)" ./cmd/portkiller/main.go
+    
+    $outputDir = "./build/release/$($target.GOOS)"
+    if (-not (Test-Path -Path $outputDir)) {
+        New-Item -ItemType Directory -Path $outputDir | Out-Null
+    }
+
+    go build -o "$outputDir/$($target.Output)" ./cmd/portkiller/main.go
 }
